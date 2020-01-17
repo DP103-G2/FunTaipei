3e3b3b214458e98e64b56c51c5ff2b45fad56565
amisl0v11 <amisl0v11@gmail.com>
2020-01-17 13:41:35 +0800

    Merge branch 'master' of /Users/chenyang/Desktop/FunTaipei with conflicts.

---
+++ app/src/main/java/com/example/funtaipei/group/GroupDetailFragment.java
@@ -0,0 +1,93 @@
+package com.example.funtaipei.group;
+
+
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.funtaipei.Common;
+import com.example.funtaipei.R;
+import com.example.funtaipei.task.ImageTask;
+
+import java.text.SimpleDateFormat;
+
+
+public class GroupDetailFragment extends Fragment {
+    private FragmentActivity activity;
+    private final static String TAG = "TAG_GroupDetailFragment";
+    private ImageView imageView;
+    private TextView tvName, tvDate, tvGroupNo, tvPeople;
+    private Button button;
+    private Group group;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = getActivity();
+        activity.setTitle("旅遊團");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = view.findViewById(R.id.imageView);
+        tvName = view.findViewById(R.id.tvName);
+        tvDate = view.findViewById(R.id.tvDate);
+        tvGroupNo = view.findViewById(R.id.tvGroupNo);
+        tvPeople = view.findViewById(R.id.tvPhone);
+
+        final NavController navController = Navigation.findNavController(view);
+        Bundle bundle = getArguments();
+        if(bundle == null || bundle.getSerializable("group") == null){
+            Common.showToast(activity,R.string.textNoGroupsFound);
+            navController.popBackStack();
+            return;
+        }
+        group = (Group) bundle.getSerializable("group");
+        showGroup();
+
+    }
+
+    private void showGroup() {
+        String url = Common.URL_SERVER + "GroupServlet";
+        int id = group.getGP_ID();
+        int imageSize = getResources().getDisplayMetrics().widthPixels/2;
+        Bitmap bitmap = null;
+        try{
+            bitmap = new ImageTask(url, id, imageSize).execute().get();
+        }catch (Exception e){
+            Log.e(TAG, e.toString());
+        }
+        if (bitmap != null){
+            imageView.setImageBitmap(bitmap);
+        }else{
+            imageView.setImageResource(R.drawable.no_image);
+        }
+        tvName.setText(group.getGP_NAME());
+        tvGroupNo.setText("團體編號：" + String.valueOf(group.getGP_ID()));
+        tvDate.setText("活動日期：" + new SimpleDateFormat("yyyy/MM/dd").format(group.getGP_EVENTDATE()));
+        //tvPeople.setText(String.valueOf(group.getGP_ENROLLMENT()));
+    }
+}
